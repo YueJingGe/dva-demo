ac4e2188d5d2a768a19ea71db8ff3cf78d66f309
wb-yjg301072 <wb-yjg301072@alibaba-inc.com>
2018-09-27 18:42:23 +0800

    理解代码一

--- dva-quickstart/src/index.js
+++ dva-quickstart/src/index.js
@@ -1,7 +1,8 @@
+// 从 'dva' 依赖中引入 dva ：
 import dva from 'dva';
 import './index.css';
 
-// 1. Initialize
+// 1. Initialize 通过函数生成一个 app 对象
 const app = dva({
   initialState: {
     products: [
@@ -11,14 +12,18 @@ const app = dva({
   }
 });
 
-// 2. Plugins
+console.log(app);
+
+// 2. Plugins 加载插件 
 // app.use({});
 
-// 3. Model
+// 3. Model 注入 model
 app.model(require('./models/products').default);
 
-// 4. Router
+// 4. Router 添加路由
 app.router(require('./router').default);
 
-// 5. Start
+// 5. Start 启动
 app.start('#root');
+
+console.log(app);
